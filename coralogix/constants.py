dcde81a0588bce3d487d59c170122dad044d0285
spathan <spathan@fortinet.com>
2024-10-04 14:24:40 +0530

    Added copyright in constants.py file

--- coralogix/constants.py
+++ coralogix/constants.py
@@ -1,3 +1,10 @@
+""""
+Copyright start
+MIT License
+Copyright (c) 2024 Fortinet Inc
+Copyright end
+"""
+
 SEARCH_ARCHIVED_LOGS_ENDPOINT = '/api/v1/dataprime/query'
 MAX_TIME_RANGE = 192
 MAX_HANDLE_RESPONSE_COUNT = 5
